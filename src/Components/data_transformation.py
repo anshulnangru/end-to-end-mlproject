492c04f684f76408466ae8f1ee016c80f7e65ab6
Anshul <anshulnangru56@gmail.com>
2025-11-21 19:05:17 +0530

    error resolved regarding the column name and save object parameter from object to obj wi=hich is its actual parameter

--- src/Components/data_transformation.py
+++ src/Components/data_transformation.py
@@ -16,27 +16,30 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    '''
-
-    This function is responsible for the data transformation based on the different types of data
     
-    '''
     preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
+    
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
+        '''
+
+    This function is responsible for the data transformation based on the different types of data
+    
+    '''
         try:
             numerical_columns=["writing_score","reading_score"]
-            categorical_columns=[
-                "gender",
-                "race_Ethnicity",
-                "parental_level_of_education",
-                "lunch",
-                "test_preaparation_course",
+            categorical_columns = [
+            "gender",
+            "race_ethnicity",
+            "parental_level_of_education",
+            "lunch",
+            "test_preparation_course",
             ]
+
 
             num_pipeline=Pipeline(
                 steps=[
@@ -49,7 +52,7 @@ class DataTransformation:
                 steps=[
                     ("imputer",SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder",OneHotEncoder()),
-                    ("scaler",StandardScaler())
+                    ("scaler",StandardScaler(with_mean=False))
                 ]
                 
             )
@@ -81,14 +84,21 @@ class DataTransformation:
 
             preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name="math_score"
-            numerical_columns=["writing_score","reading_score"]
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df=train_df.drop(columns=[target_column_name], axis=1)
-            target_feature_train_df=train_df[target_column_name]
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df=test_df[target_column_name]
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe"
@@ -97,16 +107,15 @@ class DataTransformation:
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
-            train_arr=np.c_[
-                input_feature_train_arr.np.array(target_feature_train_df)
-            ]
-            test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+            train_arr = np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+
+            test_arr = np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
 
             logging.info("Saved preprocessing object.")
 
             save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
-                object=preprocessing_obj
+                obj=preprocessing_obj
             )
             return(
                 train_arr,
